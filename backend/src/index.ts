652a81f5fc41801d1abcb3f9d19ca2e5d001476b
paupallares <ppaupallares@gmail.com>
2024-03-21 19:24:23 -0300

    validation

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -3,6 +3,7 @@ import cors from "cors"
 import "dotenv/config"
 import mongoose from "mongoose"
 import userRoutes from "./routes/users"
+import authRoutes from "./routes/auth"
 
 mongoose.connect(process.env.MONGODB_CONNECTION_STRING as string)
 .then(() => console.log("Connected to database!"));
@@ -12,6 +13,7 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use(cors())
 
+app.use("/api/auth", authRoutes)
 app.use("/api/users", userRoutes)
 
 app.listen(3000, () => {
