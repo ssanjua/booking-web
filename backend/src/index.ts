f3620eb9af50b02dd6ea69de856ae3a6b7ad256e
paupallares <ppaupallares@gmail.com>
2024-03-23 17:02:12 -0300

    create api /my-hotels

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -6,6 +6,15 @@ import userRoutes from "./routes/users";
 import authRoutes from "./routes/auth";
 import cookieParser from "cookie-parser";
 import path from "path";
+import { v2 as cloudinary } from "cloudinary";
+import myHotelRoutes from "./routes/my-hotels";
+
+
+cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET,
+})
 
 mongoose.connect(process.env.MONGODB_CONNECTION_STRING as string)
 .then(() => console.log("Connected to database!"));
@@ -20,8 +29,10 @@ app.use(cors({
 })
 )
 app.use(express.static(path.join(__dirname, "../../frontend/dist")));
+
 app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
+app.use("/api/my-hotels", myHotelRoutes)
 
 app.listen(3000, () => {
     console.log("server corriendo en :3000")
---
+++ backend/src/routes/my-hotels.ts
@@ -0,0 +1,69 @@
+import express, { Request, Response } from "express";
+import multer from "multer";
+import cloudinary from "cloudinary";
+import Hotel, { HotelType } from "../models/hotel";
+import verifyToken from "../middleware/auth";
+import { body } from "express-validator";
+
+const router = express.Router();
+
+const storage = multer.memoryStorage();
+const upload = multer({
+    storage: storage,
+    limits: {
+        fileSize: 5 * 1024 * 1024 //5mb
+    }
+})
+
+// api/my-hotels
+router.post(
+    "/", 
+    verifyToken, [
+        body("name").notEmpty().withMessage("Name is required"),
+        body("city").notEmpty().withMessage("city is required"),
+        body("country").notEmpty().withMessage("Country is required"),
+        body("description").notEmpty().withMessage("description is required"),
+        body("type").notEmpty().withMessage("type is required"),
+        body("pricePerNight")
+            .notEmpty()
+            .isNumeric()
+            .withMessage("pricePerNight is required"),
+        body("facilities")
+            .notEmpty()
+            .isArray()
+            .withMessage("facilities is required"),
+    ],
+    upload.array("imageFiles", 6), 
+    async(req: Request, res: Response) => {
+        try {
+            const imageFiles = req.files as Express.Multer.File[];
+            const newHotel: HotelType = req.body;
+
+            // upload to cloudinary
+            const uploadPromises = imageFiles.map(async(image) => {
+                const b64 = Buffer.from(image.buffer).toString("base64");
+                let dataURI = "data:" + image.mimetype + ";base64," + b64;
+                const res = await cloudinary.v2.uploader.upload(dataURI);
+                return res.url;
+            });
+
+            const imageUrls = await Promise.all(uploadPromises);
+
+            // if success, add url
+            newHotel.imageUrls = imageUrls;
+            newHotel.lastUpdated = new Date();
+            newHotel.userId = req.userId;            
+            
+            // save new hotel in db
+            const hotel = new Hotel(newHotel);
+            await hotel.save();
+
+            // return status
+            res.status(201).send(hotel);
+        } catch (error) {   
+            console.log("Error creating hotel: ", error);
+            res.status(500).json({ message: "Something went wrong"})
+        }
+});
+
+export default router;
