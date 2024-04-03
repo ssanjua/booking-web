1a7d2b9f62d7b3d6c598a3443a91ff4bdd9dae83
paupallares <ppaupallares@gmail.com>
2024-04-03 22:28:09 +0200

    star rating filter

--- frontend/src/api-client.ts
+++ frontend/src/api-client.ts
@@ -123,6 +123,11 @@ export type SearchParams = {
     adultCount?: string;
     childCount?: string;
     page?: string;
+    facilities?: string[];
+    types?: string[];
+    stars?: string[];
+    maxPrice?: string;
+    sortOption?: string;
 };
 
 export const searchHotels = async (
@@ -135,6 +140,21 @@ export const searchHotels = async (
     queryParams.append("adultCount", searchParams.adultCount || "");
     queryParams.append("childCount", searchParams.childCount || "");
     queryParams.append("page", searchParams.page || "");
+    
+    queryParams.append("maxPrice", searchParams.maxPrice || "");
+    queryParams.append("sortOption", searchParams.sortOption || "");
+
+    searchParams.facilities?.forEach((facility) => 
+        queryParams.append("facilities", facility)
+    )
+
+    searchParams.types?.forEach((type) => 
+        queryParams.append("types", type)
+    )
+
+    searchParams.stars?.forEach((star) => 
+        queryParams.append("stars", star)
+    )
 
     const response = await fetch(`${API_BASE_URL}/api/hotels/search?${queryParams}`)
 
--- frontend/src/pages/Search.tsx
+++ frontend/src/pages/Search.tsx
@@ -4,11 +4,14 @@ import * as apiClient from "../api-client";
 import { useState } from "react";
 import SearchResultsCard from "../components/SearchResultsCard";
 import Pagination from "../components/Pagination";
+import StarRatingFilter from "../components/StarRatingFilter";
 
 
 const Search = () => {
   const search = useSearchContext();
   const [page, setPage] = useState<number>(1)
+  const [selectedStars, setSelectedStars] = useState<string[]>([])
+
   const searchParams =  {
     destination: search.destination,
     checkIn: search.checkIn.toISOString(),
@@ -16,11 +19,22 @@ const Search = () => {
     adultCount: search.adultCount.toString(),
     childCount: search.childCount.toString(),
     page: page.toString(),
+    stars: selectedStars,
   }
 
   const { data: hotelData } = useQuery(["searchHotels", searchParams], () => 
     apiClient.searchHotels(searchParams)
   )
+
+  const handleStartChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const starRating = event.target.value;
+
+    setSelectedStars((prevStars) =>
+    event.target.checked
+    ? [...prevStars, starRating]
+    : prevStars.filter((star) => star !== starRating)
+    )
+  };
 
 
   return (
@@ -30,14 +44,17 @@ const Search = () => {
           <h3 className="text-lg font-semibold border-b border-slate-300 pb-5">
               Filter by
           </h3>
-          {/* TODO FILTERS */}
+          <StarRatingFilter 
+            selectedStars={selectedStars} 
+            onChange={handleStartChange} 
+          />
         </div>
       </div>
       <div className="flex flex-col gap-5">
         <div className="flex justify-between items-center">
           <span className="text-xl font-bold">
             {hotelData?.pagination.total} Hotels found
-            {search.destination ? `in ${search.destination}` : ""}
+            {search.destination ? ` in ${search.destination}` : ""}
           </span>
           { /* TODO SORT */}
         </div>
