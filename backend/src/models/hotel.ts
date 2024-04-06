c959b50244ecc02afac08389bd7125028bc09924
paupallares <ppaupallares@gmail.com>
2024-04-06 23:11:36 +0200

    stripe set up

--- backend/src/models/hotel.ts
+++ backend/src/models/hotel.ts
@@ -1,5 +1,18 @@
 import mongoose from "mongoose";
-import { HotelType } from "../shared/types";
+import { BookingType, HotelType } from "../shared/types";
+
+
+const bookingSchema = new mongoose.Schema<BookingType>({
+    firstName: { type: String, required: true },
+    lastName: { type: String, required: true },
+    email: { type: String, required: true },
+    adultCount: { type: Number, required: true },
+    childCount: { type: Number, required: true },
+    checkIn: { type: Date, required: true },
+    checkOut: { type: Date, required: true, min:1, max:5 },
+    userId: { type: String, required: true },
+    totalCost: { type: Number, required: true },
+})
 
 
 const hotelSchema = new mongoose.Schema<HotelType>({
@@ -16,6 +29,7 @@ const hotelSchema = new mongoose.Schema<HotelType>({
     starRating: { type: Number, required: true, min:1, max:5 },
     imageUrls: [{ type: String, required: true }],
     lastUpdated: { type: Date, required: true },
+    bookings: [bookingSchema],
 });
 
 const Hotel = mongoose.model<HotelType>("Hotel", hotelSchema)
