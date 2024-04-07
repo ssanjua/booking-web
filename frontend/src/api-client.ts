0d7d694a57a4d1bebdc99d96e8d327c1c7db477a
paupallares <ppaupallares@gmail.com>
2024-04-07 19:17:21 +0200

    finish stripe

--- frontend/src/api-client.ts
+++ frontend/src/api-client.ts
@@ -1,6 +1,7 @@
 import { RegisterFormData } from "./pages/Register";
 import { SignInFormData } from "./pages/SignIn";
 import { HotelSearchResponse, HotelType, PaymentIntentResponse, UserType } from "../../backend/src/shared/types";
+import { BookingFormData } from "./forms/BookingForm/BookingForm";
 
 const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || "";
 
@@ -203,4 +204,20 @@ export const createPaymentIntent = async (
     }
 
     return response.json()
+};
+
+export const createRoomBooking = async (formData: BookingFormData) => {
+    const response = await fetch(`${API_BASE_URL}/api/hotels/${formData.hotelId}/bookings`, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        credentials: "include",
+        body: JSON.stringify(formData),
+      }
+    )
+
+    if(!response.ok) {
+        throw new Error("Error booking room")
+    }
 };
--- frontend/src/forms/BookingForm/BookingForm.tsx
+++ frontend/src/forms/BookingForm/BookingForm.tsx
@@ -2,26 +2,60 @@ import { useForm } from "react-hook-form";
 import { PaymentIntentResponse, UserType } from "../../../../backend/src/shared/types";
 import { CardElement, useElements, useStripe } from "@stripe/react-stripe-js";
 import { StripeCardElement } from "@stripe/stripe-js";
+import { useSearchContext } from "../../contexts/SearchContext";
+import { useParams } from "react-router-dom";
+import { useMutation } from "react-query";
+import * as apiClient from "../../api-client";
+import { useAppContext } from "../../contexts/AppContext";
 
 type Props = {
   currentUser: UserType;
   paymentIntent: PaymentIntentResponse;
 }
 
-type BookingFormData = {
+export type BookingFormData = {
     firstName: string;
     lastName: string;
     email: string;
+    adultCount: number;
+    childCount: number;
+    checkIn: string;
+    checkOut: string;
+    hotelId: string;
+    paymentIntentId: string;
+    totalCost: number;
 }
 
 const BookingForm = ({ currentUser, paymentIntent }: Props) => {
-    const stripe = useStripe()
-    const elements = useElements()
+    const stripe = useStripe();
+    const elements = useElements();
+
+    const search = useSearchContext();
+    const { hotelId } = useParams();
+
+    const { showToast } = useAppContext();
+
+    const { mutate: bookRoom, isLoading } = useMutation(apiClient.createRoomBooking, {
+        onSuccess: () => {
+            showToast({ message: "Booking saved!", type: "SUCCESS" })
+        },
+        onError: () => {
+            showToast({ message: "Error saving booking", type: "ERROR" })
+        },
+    })
+
   const { handleSubmit, register } = useForm<BookingFormData>({
     defaultValues: {
         firstName: currentUser.firstName,
         lastName: currentUser.lastName,
         email: currentUser.email,
+        adultCount: search.adultCount,
+        childCount: search.childCount,
+        checkIn: search.checkIn.toISOString(),
+        checkOut: search.checkOut.toISOString(),
+        hotelId: hotelId,
+        totalCost: paymentIntent.totalCost,
+        paymentIntentId: paymentIntent.paymentIntentId,
     }});
 
     const onSubmit =  async (formData:BookingFormData) => {
@@ -35,11 +69,14 @@ const BookingForm = ({ currentUser, paymentIntent }: Props) => {
         )
         
         if(result.paymentIntent?.status === "succeeded") {
-            
+            bookRoom({...formData, paymentIntentId: result.paymentIntent.id })
         }
+    }
 
   return (
-    <form className="grid grid-cols-1 gap-5 ronded-lg border border-slate-300 px-5">
+    <form 
+        onSubmit={handleSubmit(onSubmit)}
+        className="grid grid-cols-1 gap-5 ronded-lg border border-slate-300 p-5">
         <span className="text-3xl font-bold">Confirm your details</span>
         <div className="grid grid-cols-2 gap-6">
             <label className="text-gray-700 text-sm font-bold flex-1">
@@ -89,6 +126,14 @@ const BookingForm = ({ currentUser, paymentIntent }: Props) => {
                 id="payment-element" 
                 className="border rounded-md p-2 text-sm"
             />
+        </div>
+        <div className="flex justify-end">
+            <button 
+                disabled={isLoading}
+                type="submit" 
+                className="bg-blue-600 text-white p-2 font-bold hover:bg-blue-500 text-md disabled:bg-gray-500">
+                {isLoading ? "Saving..." : "Confirm booking"}
+            </button>
         </div>
     </form>
   )
