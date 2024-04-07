518b07f71b8c8220b117e26ca2e2d0268c8fb09d
paupallares <ppaupallares@gmail.com>
2024-04-07 20:06:47 +0200

    better practices backend

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -42,6 +42,6 @@ app.get("*", (req: Request, res: Response) => {
   res.sendFile(path.join(__dirname, "../../frontend/dist/index.html"));
 });
 
-app.listen(7000, () => {
-  console.log("server running on localhost:7000");
+app.listen(3000, () => {
+  console.log("server running on localhost:3000");
 });
